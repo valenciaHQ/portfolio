acca093926e7771532244fc205c4017d9658bb40
Alejandro Valencia <alejandro.valencia@stackzone.com>
2022-10-05 14:44:20 -0300

    updated porfolio link

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
       <a className="text-blue-500 hover:underline" href="https://fastupload.io/en/feyBUUFfWmA5LpE/file" target="_blank" rel="noreferrer">Download my resume</a>
       <a
         className="flex items-center justify-center  m-1 p-1 sm:m-2 sm:p-2 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300"
-        href={'https://github.com/valenciaHQ/resume'}
+        href={'https://github.com/valenciaHQ/portfolio'}
       >
         <FaGithub className='text-xl' />
         <p style={{ margin: "0.5rem" }} className='text-lg'>Project repo</p>
