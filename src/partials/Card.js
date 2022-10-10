903c8995561876dcee4957dd9749e8942ca23b46
Alejandro Valencia <alejandro.valencia@stackzone.com>
2022-10-10 16:18:20 -0300

    improvemets

--- src/partials/Card.js
+++ src/partials/Card.js
@@ -28,28 +28,28 @@ function Card({ name, title, social: { github, linkedln, twitter, email } }) {
               href={github}
             >
               <FaGithub />
-              <span class="sr-only">Github</span>
+              <span className="sr-only">Github</span>
             </a>
             <a
               className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-pink-600 hover:bg-pink-600 rounded-full hover:text-white transition-colors duration-300"
               href={linkedln}
             >
               <FaLinkedinIn />
-              <span class="sr-only">Dribble</span>
+              <span className="sr-only">Linkedln</span>
             </a>
             <a
               className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-blue-500 hover:bg-blue-500 rounded-full hover:text-white transition-colors duration-300"
               href={twitter}
             >
               <FaTwitter />
-              <span class="sr-only">Twitter</span>
+              <span className="sr-only">Twitter</span>
             </a>
             <a
               className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-teal-500 hover:bg-teal-500 rounded-full hover:text-white transition-colors duration-300"
               href={"https://mail.google.com/mail/?view=cm&fs=1&to=" + email}
             >
               <FaRegEnvelope />
-              <span class="sr-only">Email</span>
+              <span className="sr-only">Email</span>
             </a>
           </div>
         </div>
