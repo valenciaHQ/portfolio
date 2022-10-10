95c18602e92ae8e4ceaf1e120f4efea58d5596ee
Alejandro Valencia <alejandro.valencia@stackzone.com>
2022-10-10 14:36:50 -0300

    added hover

--- src/partials/SectionCard.js
+++ src/partials/SectionCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SkillCard = ({ sectionName, sectionIcon }) => {
   return (
-    <div className="m-4 w-40 flex-none mx-auto text-center p-5 rounded-xl border-2 border-gray-300">
+    <div className="m-4 w-40 flex-none mx-auto text-center p-5 rounded-xl border-2 border-gray-300 hover:bg-gray-800  hover:text-white transition-colors duration-300">
       {sectionIcon}
       <p className="text-xl font-semibold mt-4">{sectionName}</p>
     </div>
