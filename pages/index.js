75f22e1fbce64aa9b8028cafa02d253e7500b3cb
Alejandro Valencia <alejandro.valencia@stackzone.com>
2022-11-09 19:44:19 -0300

    lazy loading

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import About from "../components/About";
-import Card from "../components/Card";
-import Footer from "../components/Footer";
-import Section from "../components/Section";
 import AOS from "aos";
 import data from "../data";
 import { useTheme } from 'next-themes'
+
+const About = React.lazy(() => import('../components/About'));
+const Card = React.lazy(() => import('../components/Card'));
+const Footer = React.lazy(() => import('../components/Footer'));
+const Section = React.lazy(() => import('../components/Section'));
+
 
 function App() {
     const { theme, setTheme } = useTheme()
@@ -27,14 +29,18 @@ function App() {
                 {`Toggle dark ${toogle ? "on" : "off"}`}
             </button>
             <div data-aos="fade-down" data-aos-duration="800">
-                <Card name={data.name} title={data.title} social={data.social} />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Card name={data.name} title={data.title} social={data.social} />
+                </Suspense>
             </div>
 
             <div data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
-                <About title={data.about.title} description={data.about.description} />
-                <Section data={data.skills} />
-                <Section data={data.interests} title='I like to talk about...' />
-                <Footer github={data.social.github} />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <About title={data.about.title} description={data.about.description} />
+                    <Section data={data.skills} />
+                    <Section data={data.interests} title='I like to talk about...' />
+                    <Footer github={data.social.github} />
+                </Suspense>
             </div>
         </main>
     );
